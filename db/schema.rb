d59ae040c4852075fec6c5c2eeb6fc93b24b82a0
Halisson Vitor <halissonvit@gmail.com>
2015-04-25 21:45:37 -0300

    adding bootstrap and some i18n to better styles and i18n

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,6 @@ ActiveRecord::Schema.define(version: 20150424165005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
-  enable_extension "uuid-ossp"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                          null: false
