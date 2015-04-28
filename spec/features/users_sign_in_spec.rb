154fe740a7cfe26df4dda348623ae4d4bfe31c30
Halisson <halisson.fibonacci@solucoesageis.com.br>
2015-04-28 16:56:52 -0300

    fixing sign_in feature

--- spec/features/users_sign_in_spec.rb
+++ spec/features/users_sign_in_spec.rb
@@ -2,10 +2,12 @@ require 'acceptance_spec_helper'
 
 feature 'Users Sign In' do
   let(:user) { create(:user) }
+  let(:home_page) { Pages::HomePage.new }
 
   scenario 'with valid email and password' do
     sign_in_user(user)
+    expect(home_page).to be_displayed
 
-    expect(page).to have_content 'Welcome'
+    expect(home_page).to have_content 'Welcome'
   end
 end
